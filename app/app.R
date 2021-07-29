20c4188de6f05a9f75cbc6d4c9ae685979f1fe92
claudio-a-mind <claudio.a@somosmind.com>
2021-07-29 18:28:23 -0400

    app-auth

--- app/app.R
+++ app/app.R
@@ -1,49 +1,42 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# define some credentials
+credentials <- data.frame(
+    user = c("user", "claudio"), # mandatory
+    password = c("pass", "mind2020"), # mandatory
+    #start = c("2019-04-15"), # optinal (all others)
+    #expire = c(NA, "2019-12-31"),
+    #admin = c(FALSE, TRUE),
+    #comment = "Simple and secure authentification mechanism  for single ‘Shiny’ applications.",
+    stringsAsFactors = FALSE
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+library(shiny)
+library(shinymanager)
+library(shinydashboard)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+ui <- ui <- dashboardPage(
+    dashboardHeader(title="IA Performance"),
+    dashboardSidebar(),
+    dashboardBody()
+)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Wrap your UI with secure_app
+ui <- secure_app(ui)
+
+
+server <- function(input, output, session) {
+    
+    # call the server part
+    # check_credentials returns a function to authenticate users
+    res_auth <- secure_server(
+        check_credentials = check_credentials(credentials)
+    )
+    
+    output$auth_output <- renderPrint({
+        reactiveValuesToList(res_auth)
     })
+    
+    # your classic server logic
+    
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
